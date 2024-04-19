a8a1cb2e833a62088f6222dfa6580be2303bdd11
BastianF <bastian.fuentes.gaete@gmail.com>
2024-04-19 14:02:20 -0400

    Login listo

--- src/views/auth/SignIn3View.vue
+++ src/views/auth/SignIn3View.vue
@@ -50,90 +50,46 @@ async function onSubmit() {
 
 <template>
   <!-- Page Content -->
-  <BaseBackground image="/assets/media/photos/photo28@2x.jpg">
-    <div class="row g-0 bg-primary-dark-op">
+  <!--Foto lateral en el login-->
+  <BaseBackground image="/assets/media/istyle/fondo_login.png">
+    <div class="row g-0">
       <!-- Meta Info Section -->
       <div
         class="hero-static col-lg-4 d-none d-lg-flex flex-column justify-content-center"
       >
-        <div class="p-4 p-xl-5 flex-grow-1 d-flex align-items-center">
-          <div class="w-100">
-            <RouterLink
-              :to="{ name: 'landing' }"
-              class="link-fx fw-semibold fs-2 text-white"
-            >
-              One<span class="fw-normal">UI</span>
-            </RouterLink>
-            <p class="text-white-75 me-xl-8 mt-2">
-              Welcome to your amazing app. Feel free to login and start managing
-              your projects and clients.
-            </p>
-          </div>
-        </div>
+  
         <div
           class="p-4 p-xl-5 d-xl-flex justify-content-between align-items-center fs-sm"
         >
-          <p class="fw-medium text-white-50 mb-0">
-            <strong>{{ store.app.name + " " + store.app.version }}</strong>
-            &copy; {{ store.app.copyright }}
-          </p>
-          <ul class="list list-inline mb-0 py-2">
-            <li class="list-inline-item">
-              <a class="text-white-75 fw-medium" href="javascript:void(0)"
-                >Legal</a
-              >
-            </li>
-            <li class="list-inline-item">
-              <a class="text-white-75 fw-medium" href="javascript:void(0)"
-                >Contact</a
-              >
-            </li>
-            <li class="list-inline-item">
-              <a class="text-white-75 fw-medium" href="javascript:void(0)"
-                >Terms</a
-              >
-            </li>
-          </ul>
+          
         </div>
       </div>
       <!-- END Meta Info Section -->
 
       <!-- Main Section -->
-      <div
-        class="hero-static col-lg-8 d-flex flex-column align-items-center bg-body-light"
-      >
-        <div class="p-3 w-100 d-lg-none text-center">
-          <RouterLink
-            :to="{ name: 'landing' }"
-            class="link-fx fw-semibold fs-3 text-dark"
-          >
-            One<span class="fw-normal">UI</span>
-          </RouterLink>
-        </div>
-        <div class="p-4 w-100 flex-grow-1 d-flex align-items-center">
-          <div class="w-100">
+      <div class="hero-static col-lg-8 d-flex flex-column align-items-center">
+        <div class="p-4 flex-grow-1 d-flex align-items-center">
+          <!-- Contenedor principal del logo y formulario -->
+          <div style="position: relative">
+            <img
+              src="/public/assets/media/istyle/isote separado-03.png"
+              class="imgFondo"
+            />
             <!-- Header -->
-            <div class="text-center mb-5">
-              <p class="mb-3">
-                <i class="fa fa-2x fa-circle-notch text-primary-light"></i>
-              </p>
-              <h1 class="fw-bold mb-2">Sign In</h1>
-              <p class="fw-medium text-muted">
-                Welcome, please login or
-                <RouterLink :to="{ name: 'auth-signup3' }">sign up</RouterLink>
-                for a new account.
-              </p>
+            <div class="text-center mb-3">
+              <!--   Logo  -->
+              <img src="/public/assets/media/istyle/logoIstyle.png" class="imgLgo" />
             </div>
             <!-- END Header -->
 
             <!-- Sign In Form -->
-            <div class="row g-0 justify-content-center">
+            <div class="row g-0 justify-content-center bg">
               <div class="col-sm-8 col-xl-4">
-                <form @submit.prevent="onSubmit">
+                <form @submit.prevent="onSubmit" class="form-overlay">
                   <div class="mb-4">
                     <input
                       type="text"
-                      class="form-control form-control-lg form-control-alt py-3"
+                      class="form-control"
                       id="login-username"
                       name="login-username"
                       placeholder="Username"
@@ -153,7 +109,7 @@ async function onSubmit() {
                   <div class="mb-4">
                     <input
                       type="password"
-                      class="form-control form-control-lg form-control-alt py-3"
+                      class="form-control"
                       id="login-password"
                       name="login-password"
                       placeholder="Password"
@@ -171,20 +127,12 @@ async function onSubmit() {
                     </div>
                   </div>
                   <div
-                    class="d-flex justify-content-between align-items-center mb-4"
+                    class="d-flex justify-content-center align-items-center mb-4"
                   >
                     <div>
-                      <RouterLink
-                        :to="{ name: 'auth-reminder3' }"
-                        class="text-muted fs-sm fw-medium d-block d-lg-inline-block mb-1"
-                      >
-                        Forgot Password?
-                      </RouterLink>
-                    </div>
-                    <div>
-                      <button type="submit" class="btn btn-lg btn-alt-primary">
-                        <i class="fa fa-fw fa-sign-in-alt me-1 opacity-50"></i>
-                        Sign In
+                      <button type="submit" class="btn btn-info">
+                        <i class="fa fa-fw fa-sign-in-alt me-1 "></i>
+                        Login
                       </button>
                     </div>
                   </div>
@@ -194,34 +142,33 @@ async function onSubmit() {
             <!-- END Sign In Form -->
           </div>
         </div>
-        <div
-          class="px-4 py-3 w-100 d-lg-none d-flex flex-column flex-sm-row justify-content-between fs-sm text-center text-sm-start"
-        >
-          <p class="fw-medium text-black-50 py-2 mb-0">
-            <strong>{{ store.app.name + " " + store.app.version }}</strong>
-            &copy; {{ store.app.copyright }}
-          </p>
-          <ul class="list list-inline py-2 mb-0">
-            <li class="list-inline-item">
-              <a class="text-muted fw-medium" href="javascript:void(0)"
-                >Legal</a
-              >
-            </li>
-            <li class="list-inline-item">
-              <a class="text-muted fw-medium" href="javascript:void(0)"
-                >Contact</a
-              >
-            </li>
-            <li class="list-inline-item">
-              <a class="text-muted fw-medium" href="javascript:void(0)"
-                >Terms</a
-              >
-            </li>
-          </ul>
-        </div>
       </div>
       <!-- END Main Section -->
     </div>
   </BaseBackground>
   <!-- END Page Content -->
 </template>
+
+<style>
+.imgLgo {
+  width: 50%;
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px;
+  border-radius: 10px;
+}
+.form-overlay {
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px;
+  border-radius: 10px;
+}
+.imgFondo {
+  width: 80%;
+  margin-left: 10%;
+}
+</style>
